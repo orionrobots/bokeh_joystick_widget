54281e6fcd5bdbf86950551ab029ffe8bd8d505a
Danny Staple <danny@orionrobots.co.uk>
2024-04-27 14:39:51 +0100

    Base repo layout. Not guaranteed to work yet.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bokeh Joystick Widget
+
+This is a custom widget for the Python Bokeh library that allows you to control a joystick via mouse drags or touch gestures.
+
+The widget has x and y properties that are updated as the joystick is moved. The x and y properties are in the range -1 to 1. It is self centring.
+
+The widget makes use of <https://github.com/bobboteck/JoyStick/>.
+
+## Setup
+
+## Usage
+
---
+++ src/bokeh_joystick.py
@@ -0,0 +1,41 @@
+from bokeh.models import InputWidget
+
+class Joystick(InputWidget):
+    __implementation__ = """
+        import {InputWidget, InputWidgetView} from "models/widgets/input_widget"
+        import {div} from "core/dom"
+
+        declare var JoyStick: any // This declares that JoyStick is a global variable introduced by your external JS
+
+        export class JoystickView extends InputWidgetView {
+        connect_signals(): void {
+            super.connect_signals()
+            // Connect signals here, for example, to listen for changes to model properties
+        }
+
+        render(): void {
+            super.render()
+
+            // Create a div element for the joystick and append it to the Bokeh layout
+            const joyDiv = div({id: 'joyDiv', style: {width: '200px', height: '200px'}})
+            this.el.appendChild(joyDiv)
+
+            // Instantiate the JoyStick library on this div
+            new JoyStick('joyDiv', {}, (stickData: any) => {
+            // Update the Bokeh model with the joystick position
+            this.model.x_value = stickData.xPosition
+            this.model.y_value = stickData.yPosition
+            })
+        }
+        }
+
+        export class Joystick extends InputWidget {
+        // This is where the "view" that Bokeh will use is defined
+        default_view = JoystickView
+
+        // You can define additional properties here, if needed
+        }
+    """
+    __javascript__ = [
+        "https://github.com/bobboteck/JoyStick/raw/master/joy.min.js"
+    ]
