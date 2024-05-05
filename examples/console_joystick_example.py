cf29f5bb7d47812ec8cc4c17c8bdb234e6e50d31
Danny Staple <danny@orionrobots.co.uk>
2024-05-05 20:36:44 +0100

    Formatting. exclude the vendored file.

--- examples/console_joystick_example.py
+++ examples/console_joystick_example.py
@@ -16,17 +16,20 @@ class ConsoleJoystick(Handler):
 
     def modify_document(self, doc: Document) -> None:
         # Make some dummy plot
-        plot = figure(x_range=(-1,1), y_range=(-1,1), width=500, height=500)
-        pl_x = [x*0.005 for x in range(2, 198)]
+        plot = figure(x_range=(-1, 1), y_range=(-1, 1), width=500, height=500)
+        pl_x = [x * 0.005 for x in range(2, 198)]
         pl_y = pl_x
         source = ColumnDataSource(data=dict(x=pl_x, y=pl_y))
-        plot.line('x','y', source=source, line_width=3, line_alpha=0.6, color='#ed5565')
+        plot.line(
+            "x", "y", source=source, line_width=3, line_alpha=0.6, color="#ed5565"
+        )
 
         joystick = JoystickWidget()
-        joystick.on_change('position', self.position_changed)
+        joystick.on_change("position", self.position_changed)
         doc.add_root(column(plot, joystick))
 
-server = Server({'/': Application(ConsoleJoystick())}, port=5006)
+
+server = Server({"/": Application(ConsoleJoystick())}, port=5006)
 server.start()
 print("Server started at http://localhost:5006/")
 server.run_until_shutdown()
--- examples/static_joystick_example.py
+++ examples/static_joystick_example.py
@@ -7,11 +7,11 @@ from bokeh_joystick_widget import JoystickWidget
 
 
 # Make some dummy plot
-plot = figure(x_range=(-1,1), y_range=(-1,1), width=500, height=500)
-pl_x = [x*0.005 for x in range(2, 198)]
+plot = figure(x_range=(-1, 1), y_range=(-1, 1), width=500, height=500)
+pl_x = [x * 0.005 for x in range(2, 198)]
 pl_y = pl_x
 source = ColumnDataSource(data=dict(x=pl_x, y=pl_y))
-plot.line('x','y', source=source, line_width=3, line_alpha=0.6, color='#ed5565')
+plot.line("x", "y", source=source, line_width=3, line_alpha=0.6, color="#ed5565")
 
 joystick = JoystickWidget()
 # joystick.on_change('position', lambda attr, old, new: print(f"position changed: {old} -> {new}"))
